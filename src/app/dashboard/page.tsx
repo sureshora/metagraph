a1bed91a5dbb5308ce13e02aede7601c07f537cc
Christy Lai <colorproof@gmail.com>
2023-03-14 06:50:46 -0700

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react'
 
 const Dashboard: FC = function () {
   return (
-    <div className="w-full h-screen px-6 bg-light dark:bg-dark">
+    <div className="w-full h-full px-6 pb-4 bg-light dark:bg-dark">
       <div className="mt-6 grid w-full grid-cols-1 gap-2 md:grid-cols-2 xl:grid-cols-3">
         <ClusterL0GlobalWidget />
         <ClusterL0CurrencyWidget />
@@ -22,6 +22,10 @@ const Dashboard: FC = function () {
       <div className="my-2 grid w-full grid-cols-1 gap-2 md:grid-cols-2 xl:grid-cols-2">
         <L0GlobalSnapshots />
         <L0GlobalSnapshots />
+      </div>
+      <div className="my-2 grid w-full grid-cols-1 gap-2 md:grid-cols-2 xl:grid-cols-2">
+        <L0GlobalBalances />
+        <L0GlobalBalances />
       </div>
     </div>
   )
@@ -338,6 +342,73 @@ const L0GlobalSnapshots: FC = function () {
                 f3264324c8f8dccbf119cb79a8b6172c84c781d9c9a07c90d1a4e441761a3eef
               </td>
               <td className="pb-2 pr-6">-</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
+}
+
+const L0GlobalBalances: FC = function () {
+  return (
+    <div className="rounded-lg bg-white shadow text-white dark:bg-[#40454E]">
+      <div className="flex flex-col p-6">
+        <div className="flex">
+          <div className="inline-block w-full">
+            <h3 className="text-2xl font-display dark:text-white text-white leading-[1.2rem] mb-[5px]">
+              Balances
+            </h3>
+            <span className="font-label font-medium uppercase text-xs tracking-tight text-white/50">
+              L0 Global
+            </span>
+          </div>
+          <div>
+            <svg
+              width="14"
+              height="14"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle cx="2.667" cy="2.667" r="2.667" fill="#000" />
+              <circle cx="11.334" cy="2.667" r="2.667" fill="#000" />
+              <circle cx="11.334" cy="11.334" r="2.667" fill="#000" />
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div className="overflow-x-auto">
+        <table className="mb-6 w-full table-auto text-left border-0">
+          <thead className="border-b border-black/30">
+            <tr>
+              <th className="headerRow text-white/50">Address</th>
+              <th className="headerRow text-white/50 text-right">Balance</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="tableRow">
+              <td className="dataRow text-white font-light">
+                DAG7QrKeoHbYezJ6YHUCt7PJDixWgxM4vfKz8qVT
+              </td>
+              <td className="dataRow text-white font-light text-right">
+                1,000,000,000
+              </td>
+            </tr>
+            <tr className="tableRow">
+              <td className="dataRow text-white font-light">
+                DAG7QrKeoHbYezJ6YHUCt7PJDixWgxM4vfKz8qVT
+              </td>
+              <td className="dataRow text-white font-light text-right">
+                1,000,000,000
+              </td>
+            </tr>
+            <tr className="tableRow">
+              <td className="dataRow text-white font-light">
+                DAG7QrKeoHbYezJ6YHUCt7PJDixWgxM4vfKz8qVT
+              </td>
+              <td className="dataRow text-white font-light text-right">
+                1,000,000,000
+              </td>
             </tr>
           </tbody>
         </table>
