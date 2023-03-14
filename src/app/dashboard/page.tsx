20fd2dacef4a1583c19ed93e3c84a0df96a5cfbc
Christy Lai <colorproof@gmail.com>
2023-03-14 06:16:25 -0700

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -18,9 +18,6 @@ const Dashboard: FC = function () {
         <div>
           <CardL0CurrencyFinalized />
         </div>
-        <div>
-          <CardL0CurrencyCurrent />
-        </div>
       </div>
       <div className="my-2 grid w-full grid-cols-1 gap-2 md:grid-cols-2 xl:grid-cols-2">
         <L0GlobalSnapshots />
@@ -284,45 +281,66 @@ const CardL0CurrencyFinalized: FC = function () {
   )
 }
 
-const CardL0CurrencyCurrent: FC = function () {
-  return (
-    <div className="rounded-lg bg-white shadow dark:bg-gray-800 sm:p-4 xl:p-6">
-      <div className="flex flex-col">
-        <div className="overflow-x-auto">
-          <div className="inline-block min-w-full align-middle">
-            <div className="overflow-hidden">
-              <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                L0 Currency
-              </h3>
-              <div className="my-6 grid w-full grid-cols-1 gap-6 xl:grid-cols-2">
-                <div className="text-sm"></div>
-                <div className="text-end"></div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
-
 const L0GlobalSnapshots: FC = function () {
   return (
-    <div className="rounded-lg bg-white shadow dark:bg-gray-800">
-      <div className="flex items-center justify-between">
-        <div className="p-6">
-          <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-            Snapshots
-          </h3>
-          <span>L0 Global</span>
-        </div>
-      </div>
-      <div className="flex flex-col">
-        <div className="overflow-x-auto">
-          <div className="inline-block min-w-full align-middle">
-            <div className="overflow-hidden">Table goes here</div>
+    <div className="rounded-lg bg-white shadow text-white dark:bg-[#40454E]">
+      <div className="flex flex-col p-6">
+        <div className="flex">
+          <div className="inline-block w-full">
+            <h3 className="text-2xl font-display dark:text-white text-white leading-[1.2rem] mb-[5px]">
+              Snapshots
+            </h3>
+            <span className="font-label font-medium uppercase text-xs tracking-tight text-white/50">
+              L0 Global
+            </span>
+          </div>
+          <div>
+            <svg
+              width="14"
+              height="14"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle cx="2.667" cy="2.667" r="2.667" fill="#000" />
+              <circle cx="11.334" cy="2.667" r="2.667" fill="#000" />
+              <circle cx="11.334" cy="11.334" r="2.667" fill="#000" />
+            </svg>
           </div>
         </div>
+      </div>
+      <div className="overflow-x-auto">
+        <table className="mb-6 w-full table-auto text-left border-0">
+          <thead className="border-b border-black/30">
+            <tr>
+              <th className="headerRow text-white/50">Ordinal</th>
+              <th className="headerRow text-white/50">Snapshot Hash</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="tableRow">
+              <td className="dataRow text-white font-light">32</td>
+              <td className="dataRow text-white font-light">
+                f3264324c8f8dccbf119cb79a8b6172c84c781d9c9a07c90d1a4e441761a3eef
+              </td>
+              <td className="pb-2 pr-6">-</td>
+            </tr>
+            <tr className="tableRow">
+              <td className="dataRow text-white font-light">31</td>
+              <td className="dataRow text-white font-light">
+                f3264324c8f8dccbf119cb79a8b6172c84c781d9c9a07c90d1a4e441761a3eef
+              </td>
+              <td className="pb-2 pr-6">-</td>
+            </tr>
+            <tr className="tableRow">
+              <td className="dataRow text-white font-light">30</td>
+              <td className="dataRow text-white font-light">
+                f3264324c8f8dccbf119cb79a8b6172c84c781d9c9a07c90d1a4e441761a3eef
+              </td>
+              <td className="pb-2 pr-6">-</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   )
