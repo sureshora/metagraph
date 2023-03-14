e3c9496a128b6e95c1288deee589539782dabc41
Christy Lai <colorproof@gmail.com>
2023-03-14 08:12:17 -0700

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -21,7 +21,7 @@ const Dashboard: FC = function () {
       </div>
       <div className="my-2 grid w-full grid-cols-1 gap-2 md:grid-cols-2 xl:grid-cols-2">
         <L0GlobalSnapshots />
-        <L0GlobalSnapshots />
+        <L0CurrencySnapshots />
       </div>
       <div className="my-2 grid w-full grid-cols-1 gap-2 md:grid-cols-2 xl:grid-cols-2">
         <L0GlobalBalances />
@@ -413,6 +413,146 @@ const L0GlobalSnapshots: FC = function () {
                       d="M9.397 8.654a.525.525 0 0 1 0 .743L6.793 12l2.604 2.603a.525.525 0 0 1-.743.743L5.68 12.372a.525.525 0 0 1 0-.743l2.975-2.975a.525.525 0 0 1 .743 0Zm5.207 0a.525.525 0 0 1 .743 0l2.975 2.975a.525.525 0 0 1 0 .743l-2.975 2.975a.525.525 0 1 1-.743-.743l2.604-2.603-2.604-2.604a.525.525 0 0 1 0-.743ZM12.965 6.408c.285.05.477.32.428.607l-1.75 10.15a.525.525 0 0 1-1.035-.179l1.75-10.15a.525.525 0 0 1 .607-.428Z"
                       fill="currentColor"
                       className="fill-white/70"
+                    />
+                  </svg>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
+}
+
+const L0CurrencySnapshots: FC = function () {
+  return (
+    <div className="rounded-lg bg-[#B9DD6D] shadow text-black">
+      <div className="flex flex-col p-6">
+        <div className="flex">
+          <div className="inline-block w-full">
+            <h3 className="text-2xl font-display text-black leading-[1.2rem] mb-[5px]">
+              Snapshots
+            </h3>
+            <span className="font-label font-medium uppercase text-xs tracking-tight text-black/50">
+              L0 Currency
+            </span>
+          </div>
+          <div>
+            <svg
+              width="14"
+              height="14"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle cx="2.667" cy="2.667" r="2.667" fill="#000" />
+              <circle cx="11.334" cy="2.667" r="2.667" fill="#000" />
+              <circle cx="11.334" cy="11.334" r="2.667" fill="#000" />
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div className="overflow-x-auto">
+        <table className="mb-6 w-full table-auto text-left border-0">
+          <thead className="border-b border-black/30">
+            <tr>
+              <th className="headerRow text-black/50">Ordinal</th>
+              <th className="headerRow text-black/50">Snapshot Hash</th>
+              <th className="headerRow text-black/50 text-right">JSON</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="tableRow">
+              <td className="dataRow">32</td>
+              <td className="dataRow">
+                f3264324c8f8dccbf119cb79a8b6172c84c781d9c9a07c90d1a4e441761a3eef
+              </td>
+              <td className="pr-6">
+                <div>
+                  <svg
+                    width="24"
+                    height="24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="float-right"
+                  >
+                    <rect
+                      width="24"
+                      height="24"
+                      rx="12"
+                      fill="currentColor"
+                      className="fill-black/10"
+                    />
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M9.397 8.654a.525.525 0 0 1 0 .743L6.793 12l2.604 2.603a.525.525 0 0 1-.743.743L5.68 12.372a.525.525 0 0 1 0-.743l2.975-2.975a.525.525 0 0 1 .743 0Zm5.207 0a.525.525 0 0 1 .743 0l2.975 2.975a.525.525 0 0 1 0 .743l-2.975 2.975a.525.525 0 1 1-.743-.743l2.604-2.603-2.604-2.604a.525.525 0 0 1 0-.743ZM12.965 6.408c.285.05.477.32.428.607l-1.75 10.15a.525.525 0 0 1-1.035-.179l1.75-10.15a.525.525 0 0 1 .607-.428Z"
+                      fill="currentColor"
+                      className="fill-black/70"
+                    />
+                  </svg>
+                </div>
+              </td>
+            </tr>
+            <tr className="tableRow">
+              <td className="dataRow">31</td>
+              <td className="dataRow">
+                f3264324c8f8dccbf119cb79a8b6172c84c781d9c9a07c90d1a4e441761a3eef
+              </td>
+              <td className="pr-6">
+                <div>
+                  <svg
+                    width="24"
+                    height="24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="float-right"
+                  >
+                    <rect
+                      width="24"
+                      height="24"
+                      rx="12"
+                      fill="currentColor"
+                      className="fill-black/10"
+                    />
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M9.397 8.654a.525.525 0 0 1 0 .743L6.793 12l2.604 2.603a.525.525 0 0 1-.743.743L5.68 12.372a.525.525 0 0 1 0-.743l2.975-2.975a.525.525 0 0 1 .743 0Zm5.207 0a.525.525 0 0 1 .743 0l2.975 2.975a.525.525 0 0 1 0 .743l-2.975 2.975a.525.525 0 1 1-.743-.743l2.604-2.603-2.604-2.604a.525.525 0 0 1 0-.743ZM12.965 6.408c.285.05.477.32.428.607l-1.75 10.15a.525.525 0 0 1-1.035-.179l1.75-10.15a.525.525 0 0 1 .607-.428Z"
+                      fill="currentColor"
+                      className="fill-black/70"
+                    />
+                  </svg>
+                </div>
+              </td>
+            </tr>
+            <tr className="tableRow">
+              <td className="dataRow">30</td>
+              <td className="dataRow">
+                f3264324c8f8dccbf119cb79a8b6172c84c781d9c9a07c90d1a4e441761a3eef
+              </td>
+              <td className="pr-6">
+                <div>
+                  <svg
+                    width="24"
+                    height="24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="float-right"
+                  >
+                    <rect
+                      width="24"
+                      height="24"
+                      rx="12"
+                      fill="currentColor"
+                      className="fill-black/10"
+                    />
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M9.397 8.654a.525.525 0 0 1 0 .743L6.793 12l2.604 2.603a.525.525 0 0 1-.743.743L5.68 12.372a.525.525 0 0 1 0-.743l2.975-2.975a.525.525 0 0 1 .743 0Zm5.207 0a.525.525 0 0 1 .743 0l2.975 2.975a.525.525 0 0 1 0 .743l-2.975 2.975a.525.525 0 1 1-.743-.743l2.604-2.603-2.604-2.604a.525.525 0 0 1 0-.743ZM12.965 6.408c.285.05.477.32.428.607l-1.75 10.15a.525.525 0 0 1-1.035-.179l1.75-10.15a.525.525 0 0 1 .607-.428Z"
+                      fill="currentColor"
+                      className="fill-black/70"
                     />
                   </svg>
                 </div>
