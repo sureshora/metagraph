beef9238ef7780f3ee7c40614f629b61b187161c
Christy Lai <colorproof@gmail.com>
2023-03-14 09:07:08 -0700

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -13,10 +13,7 @@ const Dashboard: FC = function () {
           <CardL0GlobalLatest />
         </div>
         <div>
-          <CardL0GlobalCurrent />
-        </div>
-        <div>
-          <CardL0CurrencyFinalized />
+          <CardL0GlobalLatest />
         </div>
       </div>
       <div className="my-2 grid w-full grid-cols-1 gap-2 md:grid-cols-2 xl:grid-cols-2">
@@ -214,72 +211,31 @@ const ClusterL1CurrencyWidget: FC = function () {
 
 const CardL0GlobalLatest: FC = function () {
   return (
-    <div className="rounded-lg bg-white text-gray-900 shadow dark:bg-[#40454E] dark:text-white">
-      <div className="flex flex-col sm:p-4 xl:p-6">
-        <div className="overflow-x-auto">
-          <div className="inline-block min-w-full align-middle">
-            <div className="overflow-hidden">
-              <div className="grid w-full grid-cols-1 gap-6 xl:grid-cols-2">
-                <div>
-                  <h3 className="text-lg font-semibold">L0 Global</h3>
-                </div>
-                <div className="text-end">1s</div>
-              </div>
-              <div className="grid w-full grid-cols-1 text-center">
-                <div className="text-[54px]">32</div>
-                <div>Latest Snapshot</div>
-              </div>
+    <div className="rounded-lg bg-white shadow text-white dark:bg-[#40454E]">
+      <div className="flex flex-col p-6">
+        <div className="flex">
+          <div className="inline-block w-full">
+            <h3 className="text-lg font-display dark:text-white text-white leading-[1.2rem] mb-[5px]">
+              L0 Global
+            </h3>
+            <div className="grid w-full grid-cols-1 text-center pt-2 pb-8">
+              <div className="text-[54px]">33</div>
+              <div>Latest Snapshot</div>
             </div>
           </div>
-        </div>
-      </div>
-    </div>
-  )
-}
-
-const CardL0GlobalCurrent: FC = function () {
-  return (
-    <div className="rounded-lg bg-white text-gray-900 shadow dark:bg-[#40454E] dark:text-white">
-      <div className="flex flex-col sm:p-4 xl:p-6">
-        <div className="overflow-x-auto">
-          <div className="inline-block min-w-full align-middle">
-            <div className="overflow-hidden">
-              <h3 className="text-lg font-semibold">L0 Global</h3>
-              <div className="grid w-full grid-cols-1 text-center">
-                <div className="text-[54px]">33</div>
-                <div>Current Snapshot</div>
-              </div>
-            </div>
+          <div>
+            <svg
+              width="14"
+              height="14"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle cx="2.667" cy="2.667" r="2.667" fill="#000" />
+              <circle cx="11.334" cy="2.667" r="2.667" fill="#000" />
+              <circle cx="11.334" cy="11.334" r="2.667" fill="#000" />
+            </svg>
           </div>
         </div>
-      </div>
-      <div className="grid w-full grid-cols-1 gap-6 border-t border-black/30 px-6 py-3 xl:grid-cols-2">
-        <div className="text-sm">.</div>
-        <div className="text-end">.</div>
-      </div>
-    </div>
-  )
-}
-
-const CardL0CurrencyFinalized: FC = function () {
-  return (
-    <div className="rounded-lg bg-white text-gray-900 shadow dark:bg-[#B9DD6D] dark:text-black">
-      <div className="flex flex-col sm:p-4 xl:p-6">
-        <div className="overflow-x-auto">
-          <div className="inline-block min-w-full align-middle">
-            <div className="overflow-hidden">
-              <h3 className="text-lg font-semibold">L0 Currency</h3>
-              <div className="grid w-full grid-cols-1 text-center">
-                <div className="text-[54px]">14</div>
-                <div>Finalized Snapshot</div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="grid w-full grid-cols-1 gap-6 border-t border-black/20 px-6 py-3 xl:grid-cols-2">
-        <div className="text-sm">Total Supply</div>
-        <div className="text-end">6,000,000,000</div>
       </div>
     </div>
   )
@@ -463,7 +419,7 @@ const L0CurrencySnapshots: FC = function () {
           </thead>
           <tbody>
             <tr className="tableRow">
-              <td className="dataRow">32</td>
+              <td className="dataRow">14</td>
               <td className="dataRow">
                 f3264324c8f8dccbf119cb79a8b6172c84c781d9c9a07c90d1a4e441761a3eef
               </td>
@@ -495,7 +451,7 @@ const L0CurrencySnapshots: FC = function () {
               </td>
             </tr>
             <tr className="tableRow">
-              <td className="dataRow">31</td>
+              <td className="dataRow">13</td>
               <td className="dataRow">
                 f3264324c8f8dccbf119cb79a8b6172c84c781d9c9a07c90d1a4e441761a3eef
               </td>
@@ -527,7 +483,7 @@ const L0CurrencySnapshots: FC = function () {
               </td>
             </tr>
             <tr className="tableRow">
-              <td className="dataRow">30</td>
+              <td className="dataRow">12</td>
               <td className="dataRow">
                 f3264324c8f8dccbf119cb79a8b6172c84c781d9c9a07c90d1a4e441761a3eef
               </td>
